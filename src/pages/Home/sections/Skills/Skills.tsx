21510822442943ba53c5e492945d87d262117408
marcos-valle <marcos.vallesn@gmail.com>
2025-06-08 03:04:47 -0300

    style: add style to skills section

--- src/pages/Home/sections/Skills/Skills.tsx
+++ src/pages/Home/sections/Skills/Skills.tsx
@@ -5,15 +5,29 @@ const Skills = () => {
 
   return (
 
-    <section className="flex flex-col items-center mt-28">
-      <h1 className="text-4xl font-extrabold text-center">HABILIDADES</h1>
-      <h3 className="text-xl font-semibold text-center mt-1 bg-gradient-to-r from-violet-600 to-orange-400 bg-clip-text text-transparent inline-block">TECNOLOGIAS</h3>
+    <section id="skills-section" className="flex flex-col items-center mt-28">
 
-      <div>
-        <p>JAVA</p>
-        <p>React</p>
-        <p>JavaScript</p>
-      </div>
+        <div id="skills-header" className="flex flex-col items-center">
+            <h1 className="text-4xl font-extrabold">HABILIDADES</h1>
+            <h3 className="text-sm font-semibold tracking-widest bg-gradient-to-r from-violet-600 to-orange-400 bg-clip-text text-transparent inline-block">TECNOLOGIAS</h3>
+        </div>  
+
+        <div id="skills-content">
+            <div id="row-1" className="flex justify-center items-center gap-4 mt-8">
+                <p className="border-1 border-indigo-700 rounded-full px-8 py-2 bg-black text-gray-300">Typescript</p>
+                <p className="border-1 border-indigo-700 rounded-full px-8 py-2 bg-black text-gray-300">Typescript</p>
+                <p className="border-1 border-indigo-700 rounded-full px-8 py-2 bg-black text-gray-300">Typescript</p>
+                <p className="border-1 border-indigo-700 rounded-full px-8 py-2 bg-black text-gray-300">Typescript</p>
+                <p className="border-1 border-indigo-700 rounded-full px-8 py-2 bg-black text-gray-300">Typescript</p>
+                
+            </div>
+            <div id="row-2" className="flex justify-center items-center gap-4 mt-4">
+                <p className="border-1 border-indigo-700 rounded-full px-8 py-2 bg-black text-gray-300">Typescript</p>
+                <p className="border-1 border-indigo-700 rounded-full px-8 py-2 bg-black text-gray-300">Typescript</p>
+                <p className="border-1 border-indigo-700 rounded-full px-8 py-2 bg-black text-gray-300">Typescript</p>
+                <p className="border-1 border-indigo-700 rounded-full px-8 py-2 bg-black text-gray-300">Typescript</p>
+            </div>
+        </div>
     </section>
     
   )
